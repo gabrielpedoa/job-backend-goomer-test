84a7405245154ad7b44a850af3390033a23aff74
gabrieldev <gabrieldev.abmaxeducacional@gmail.com>
2024-03-14 16:10:37 -0300

    feat: add schemaValidator

--- src/presentational/validator/schemaValidator.ts
+++ src/presentational/validator/schemaValidator.ts
@@ -1,3 +1,16 @@
-export class SchemaValidator {
-    
+import { ZodObject } from "zod";
+
+export class SchemaValidator<T> {
+  private schema: ZodObject<any>;
+  constructor(schema: ZodObject<any>) {
+    this.schema = schema;
+  }
+
+  public isValid(data: T) {
+    console.log(data);
+    const isSchemaValid = this.schema.safeParse(data);
+    if (isSchemaValid.success) return null;
+    console.log(isSchemaValid.error.errors);
+    return isSchemaValid.error.errors[0].message;
+  }
 }
